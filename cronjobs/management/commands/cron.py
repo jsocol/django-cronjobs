f519aa347bec145ab34df9b86b6a7f29bd10ead6
Dave Dash <dd+github@davedash.com>
2011-05-09 14:42:38 -0700

    Locking

--- cronjobs/management/commands/cron.py
+++ cronjobs/management/commands/cron.py
@@ -1,5 +1,9 @@
-import sys
+import atexit
 import logging
+import os
+import sys
+import tempfile
+
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -30,8 +34,27 @@ class Command(BaseCommand):
 
         script, args = args[0], args[1:]
         if script not in registered:
-            log.error("Cron called with unrecognized command: %s %s" % (script, args))
+            log.error("Cron called with unrecognized command: %s %s" %
+                      (script, args))
             print 'Unrecognized name: %s' % script
+            sys.exit(1)
+
+        # aquire lock
+        filename = os.path.join(tempfile.gettempdir(),
+                                'django_cron.%s' % script)
+        try:
+            fd = os.open(filename, os.O_CREAT|os.O_EXCL)
+
+            def register():
+                os.close(fd)
+                os.remove(filename)
+
+            atexit.register(register)
+        except OSError:
+            msg = ("Script run multiple times. If this isn't true, delete "
+                   "`%s`." % filename)
+            log.error(msg)
+            sys.stderr.write(msg + "\n")
             sys.exit(1)
 
         log.info("Beginning job: %s %s" % (script, args))
